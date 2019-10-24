221f613de872dfd22e40d37cc20d55bf90cbb420
yaofei <fei.yao@tellyes.com>
2019-10-24 17:20:50 +0800

    公共上传、接口和主页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,8 +1,45 @@
+const {http, api, tips} = require('../../utils/util.js');
 Page({
-  data: {
-    background: ['../../images/1.jpg', '../../images/1.jpg']
-  },
-  onLoad: function () {
-    
-  }
-})
+    data: {
+        background: [],
+        news: []
+    },
+    onLoad: function () {
+        let _this = this;
+        // 获取轮播图
+        http({
+            url: api.getBannerList,
+            data: {
+                pageNum: 1,
+                pageSize: 5,
+                status: 1
+            },
+            success(res) {
+                if (res.code === "0") {
+                    _this.setData({
+                        background: res.result
+                    });
+                } else {
+                    tips(res.message);
+                }
+            }
+        });
+        // 获取新闻列表
+        http({
+            url: api.getNewsList,
+            data: {
+                pageNum: 1,
+                pageSize: 2
+            },
+            success(res) {
+                if (res.code === "0") {
+                    _this.setData({
+                        news: res.result
+                    });
+                } else {
+                    tips(res.message);
+                }
+            }
+        });
+    }
+});
--- utils/util.js
+++ utils/util.js
@@ -1,19 +1,36 @@
-const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
+const http = (params, load = true) => {
+    load && wx.showLoading({
+        title: '加载中...',
+        mask: true
+    });
+    wx.request({
+        url: params.url, //仅为示例，并非真实的接口地址
+        data: params.data,
+        header: {
+            'content-type': 'application/json' // 默认值
+        },
+        success(res) {
+            load && wx.hideLoading();
+            params.success(res.data);
+        }
+    })
+};
 
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
-}
+const tips = (message, type = "none", time = 2000) => {
+    wx.showToast({
+        title: message,
+        icon: type,
+        mask: true,
+        duration: time
+    })
+};
 
-const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : '0' + n
-}
+const url = "https://www.sqyj.tech/schoolhome-server/";
+const api = {
+    getBannerList: url + "banner/getBannerList.do?",// 获取轮播图
+    getNewsList: url + "news/getNewsList.do?",// 获取新闻
+};
 
 module.exports = {
-  formatTime: formatTime
-}
+    http, api, tips
+};
